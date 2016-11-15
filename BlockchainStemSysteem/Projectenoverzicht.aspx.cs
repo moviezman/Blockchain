c474b5ee5947fb8d498685c739383a25daf6a730
Michiel <michielmetmij@hotmail.com>
2016-11-15 11:15:51 +0100

    Verificatie van of stemmer bestaat gefixt.

--- BlockchainStemSysteem/Projectenoverzicht.aspx.cs
+++ BlockchainStemSysteem/Projectenoverzicht.aspx.cs
@@ -10,7 +10,10 @@ public partial class Projectenoverzicht : System.Web.UI.Page
     protected void Page_Load(object sender, EventArgs e)
     {
         Stemming Test = new Stemming("Test", 2);
-        if (Test.Stemmers.Exists(x => x.UniekeCode != Request.QueryString["Stemmer"]))
+        if (Test.Stemmers.Exists(x => x.UniekeCode == Request.QueryString["Stemmer"]))
+        {
+            
+        } else
         {
             //Redirect naar de inlogpagina als de unieke code van de stemmer niet bestaat
             Response.Redirect("Inlogpagina");
