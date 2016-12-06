02ee0207ffe0bf2564a3c3415daf0c5c0db4fd46
THIJSLAPTOP\thijs <personalthijsiedema@gmail.com>
2016-12-06 15:47:23 +0100

    SMS wordt verstuurd, controle of nummer al is ingevoerd

--- BlockchainStemSysteem/Codeuitgeven.aspx.cs
+++ BlockchainStemSysteem/Codeuitgeven.aspx.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Data.SqlClient;
 using System.Linq;
+using System.Net.Mail;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
@@ -15,41 +16,80 @@ public partial class Codeuitgeven : System.Web.UI.Page
 
     protected void Button1_Click(object sender, EventArgs e)
     {
+
+        DatabaseConnectie dbconnect = new DatabaseConnectie();
+        SqlConnection sqlConnection = new SqlConnection(dbconnect.dbConnectie);
+
+        // query ophalen laatste code
+        SqlCommand NieuweCodeQuery = new SqlCommand("SELECT Top 1 UniekeCode FROM UC WHERE Ingezet = 'false'", sqlConnection);
+        // nummer toevoegen aan database
+        SqlCommand UpdateNummer = new SqlCommand("INSERT INTO Stemmer VALUES(" + TextBox1.Text + ", 'true');",sqlConnection);
+        // Kijkt of telefoonnnummer al is ingevoerd
+        SqlCommand ZoekNummer = new SqlCommand("SELECT COUNT (HeeftStem) as Aantal FROM Stemmer WHERE TelefoonNummer = '" + TextBox1.Text + "';",sqlConnection);
+
         Nummercontrole check = new Nummercontrole();
 
-        if (check.Nummercheck(Convert.ToInt32(TextBox1.Text)))
+        //Checken of een telefoonnummer wel een geldig nummer is
+        //if (check.Nummercheck(Convert.ToInt32(TextBox1.Text)))
+        //hieronder een tijdelijke check 
+        if (TextBox1.Text != "")
         {
-
-            DatabaseConnectie dbconnect = new DatabaseConnectie();
-            SqlConnection sqlConnection = new SqlConnection(dbconnect.dbConnectie);
-
-
-            // test SELECT Top 1 UniekeCode FROM UC WHERE Ingezet = 'false');
-            SqlCommand NieuweCodeQuery = new SqlCommand("SELECT Top 1 UniekeCode FROM UC WHERE Ingezet = 'false'", sqlConnection);
-
-            string NieuweCode = "";
 
             sqlConnection.Open();
 
-            NieuweCode = Convert.ToString(NieuweCodeQuery.ExecuteScalar());
+            int Bestaandnummer = Convert.ToInt32(ZoekNummer.ExecuteScalar());
 
-            sqlConnection.Close();
-
-            if (NieuweCode != null)
+            //kijken of een nummer al gebruikt is
+            if (Bestaandnummer == 0)
             {
-                Label4.Visible = true;
-                Label4.Text = "Uw code is: " + NieuweCode;
+
+                UpdateNummer.ExecuteNonQuery();
+
+                string password = "Blockchain123";
+
+
+
+                string NieuweCode = Convert.ToString(NieuweCodeQuery.ExecuteScalar());
+
+                sqlConnection.Close();
+
+                if (NieuweCode != null)
+                {
+
+                    MailMessage mail = new MailMessage();
+                    SmtpClient SmtpServer = new SmtpClient("smtp.gmail.com");
+
+                    mail.From = new MailAddress("winnovationwindesheim@gmail.com");
+                    //mail.To.Add(TextBox1.Text + "@sms.informaxion.nl");
+                    mail.To.Add("personalthijsiedema@gmail.com");
+                    mail.Subject = "2410, Winnovation";
+                    //mail.Body = "Hier is uw code voor de Winnovation: http://www.winnovationwindesheim.nl/projectenoverzicht?" + NieuweCode;
+                    mail.Body = "Hier is uw code voor de Winnovation: localhost:50512/projectenoverzicht?Stemmer=" + NieuweCode;
+
+                    SmtpServer.Port = 587;
+                    SmtpServer.Credentials = new System.Net.NetworkCredential("winnovationwindesheim", password);
+                    SmtpServer.EnableSsl = true;
+
+                    SmtpServer.Send(mail);
+                    Label4.Text = "mail Send";
+
+                    Label4.Visible = true;
+                    Label4.Text += " Hier is uw code voor de Winnovation: localhost:50512/projectenoverzicht?Stemmer=" + NieuweCode;
+
+                }
             }
             else
             {
                 Label4.Visible = true;
-                Label4.Text = "U hebt een ongeldig nummer ingevoerd";
+                Label4.Text = "Dit nummer is al gebruikt + " + Bestaandnummer;
             }
-        } else
+        }
+
+        else
         {
             Label4.Visible = true;
-            Label4.Text = "nummer moet 5 zijn";
+            Label4.Text = "Het nummer is ongeldig";
         }
-        
+
     }
 }
