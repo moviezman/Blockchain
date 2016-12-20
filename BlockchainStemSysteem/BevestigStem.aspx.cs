1f8960de027578818ff498dcc476b15d8499410d
THIJSLAPTOP\thijs <personalthijsiedema@gmail.com>
2016-12-20 12:29:41 +0100

    hlj

--- BlockchainStemSysteem/BevestigStem.aspx.cs
+++ BlockchainStemSysteem/BevestigStem.aspx.cs
@@ -9,15 +9,19 @@ public partial class BevestigStem : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
-
+        string Team = Request.QueryString["GestemdOp"];
+        Label1.Text = Team;
     }
 
     protected void ButtonJa_Click(object sender, EventArgs e)
     {
-
+        string Team = Request.QueryString["GestemdOp"];
+        Response.Redirect("Gestemd.aspx?GestemdOp=" + Team );
     }
     protected void ButtonNee_Click(object sender, EventArgs e)
     {
+        string StemCode = Request.QueryString["StemCode"];
+        Response.Redirect("Projectenoverzicht.aspx?StemCode=" + StemCode);
 
     }
 }
