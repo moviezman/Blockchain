467edc6df2b5b8a078c7614d472c6dad96f93fd8
Michiel <michielmetmij@hotmail.com>
2016-12-27 13:44:46 +0100

    Begin van blockchainklasse gemaakt

---
+++ BlockchainStemSysteem/App_Code/Blocks.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for Blocks
+/// </summary>
+public static class Blocks
+{
+    public static string MaakBlock(string Stemming)
+    {
+        string blockstring = "";
+
+
+
+
+
+
+
+
+        return blockstring;
+    }
+}
